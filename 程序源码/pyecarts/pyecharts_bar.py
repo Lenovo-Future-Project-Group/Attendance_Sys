23c21b3c1c0ac0bad045130c3106f276fbba6423
HKBingJohn <bingjohn.max@gmail.com>
2022-09-07 09:54:06 +0800

    Initial commit

--- 程序源码/pyecarts/pyecharts_bar.py
+++ 程序源码/pyecarts/pyecharts_bar.py
@@ -8,10 +8,10 @@ from pyecharts import options as opts
 def bar_base() -> Bar:
     data = (
         Bar()
-        .add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
-        .add_yaxis("商家A", [randrange(0, 100) for _ in range(6)])
-        .add_yaxis("商家B", [randrange(0, 100) for _ in range(6)])
-        .set_global_opts(title_opts=opts.TitleOpts(title="Bar-基本示例", subtitle="我是副标题"))
+        .add_xaxis(['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子'])
+        .add_yaxis('商家A', [randrange(0, 100) for _ in range(6)])
+        .add_yaxis('商家B', [randrange(0, 100) for _ in range(6)])
+        .set_global_opts(title_opts=opts.TitleOpts(title='Bar-基本示例', subtitle='我是副标题'))
     )
     return data
 
